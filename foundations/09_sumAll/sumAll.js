03075840e4cbb36b2a203aa5f96f8c32e902e287
Ruby-YSY <56031438+Ruby-YSY@users.noreply.github.com>
2026-03-11 10:01:08 +0000

    Loops and arrays: exercise 9 completed

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  function isValidNumber(num) {
+    return Number.isInteger(num) && num >= 0;
+  }
 
+  if (!isValidNumber(num1) || !isValidNumber(num2)) {
+    return "ERROR";
+  }
+
+  if (num1 > num2) {
+    [num1, num2] = [num2, num1];
+  }
+
+  let sum = 0;
+  for (let i = num1; i <= num2; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
